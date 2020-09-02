974e878b48b7a287d74250c1cce0ab24c292cbd5
Vitaliy Zhyrytskyy <Vitaliy_Zhyrytskyy@epam.com>
2020-09-02 13:36:28 +0300

    task3-comments

--- src/app/core/services/constants.service.ts
+++ src/app/core/services/constants.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root',
+  // эти же значения можно сразу в классе задать...
+  // меньше кода, проще читать.
   useValue: {
     app: 'ShopApp',
     ver: '1.0',
