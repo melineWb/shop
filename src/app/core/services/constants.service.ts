4b270b4d55e15444b1dc335a44268d8564f9e126
KYIV\Maryna_Semiroz <Maryna_Semiroz@epam.com>
2020-09-05 21:12:19 +0300

    fix comments

--- src/app/core/services/constants.service.ts
+++ src/app/core/services/constants.service.ts
@@ -1,18 +1,11 @@
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root',
-  // эти же значения можно сразу в классе задать...
-  // меньше кода, проще читать.
-  useValue: {
-    app: 'ShopApp',
-    ver: '1.0',
-    apiUrl: 'http://localhost:4200/'
-  }
+  providedIn: 'root'
 })
 
 export class ConstantsService {
-  app: string;
-  ver: string;
-  apiUrl: string;
+  app = 'ShopApp';
+  ver = '1.0';
+  apiUrl = 'http://localhost:4200/';
 }
--- src/app/shared/components/availability-flag/availability-flag.component.ts
+++ src/app/shared/components/availability-flag/availability-flag.component.ts
@@ -1,22 +1,16 @@
-import { Component, OnInit, Input, OnChanges } from '@angular/core';
+import { Component, Input, OnChanges } from '@angular/core';
 
 @Component({
   selector: 'app-availability-flag',
   templateUrl: './availability-flag.component.html',
   styleUrls: ['./availability-flag.component.less']
 })
-export class AvailabilityFlagComponent implements OnInit, OnChanges {
+export class AvailabilityFlagComponent implements OnChanges {
   text: string;
   @Input() isAvailable: boolean;
 
   constructor() { }
 
-  ngOnInit(): void {
-    this.setDataFlag();
-  }
-
-  // первый раз выполниться перед ngOnInit()
-  // может быть не надо вызывать в ngOnInit()
   ngOnChanges(): void {
     this.setDataFlag();
   }
